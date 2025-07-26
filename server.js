ec880302834d6daea0a62761c0861b4592f4f902
shashwat shukla <shuklashashwat009@gmail.com>
2025-07-27 01:03:56 +0530

    cors update

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import chipRoutes from "./routes/chipRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+}));
 app.use(express.json());
 
 app.use("/api/chips", chipRoutes);
